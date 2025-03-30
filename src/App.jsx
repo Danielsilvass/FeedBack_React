f2f464d6eaadca51b657691ea88ca81161a17a8d
Daniel silva <danielssilvas123@gmail.com>
2025-03-30 17:51:16 +0000

    Fim da ciração e estilização do segunda card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import iconStar from "../public/icon-star.svg"
-
+import Illustrationthankyou from "../public/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App() {
@@ -42,7 +42,12 @@ export function App() {
     </div>
     ) : (
     <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
-      
+      <img className="mx-auto mb-6" src={Illustrationthankyou} alt="Illustration thank you" />
+      <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selected {AlteraValor} out of 5</p>
+
+      <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
+
+      <p className="text-sm text-light-grey leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
     </div>)
   )
 }
